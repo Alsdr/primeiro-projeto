0e247196405f91ae501f66f656b3740a18dd5cf4
alsdr <alessandrdm@gmail.com>
2025-05-27 21:08:40 -0400

    Vai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NomeComponenteComponent } from './nome-componente/nome-componente.component';
+import { PerfilComponent } from "./perfil/perfil.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NomeComponenteComponent],
+  imports: [RouterOutlet, NomeComponenteComponent, PerfilComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
